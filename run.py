16b984c5525d177cd2f20fc451bc05322fa1041c
Johneriksson88 <john.e.eriksson@gmail.com>
2022-10-11 21:16:06 +0000

    Add jumbotron and product cards in index.html

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("base.html", page_title="Home")
+    return render_template("index.html", page_title="Home")
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,12 @@
 const toggleButton = document.getElementsByClassName("toggle-btn")[0]
 const navBarLinks = document.getElementsByClassName("navbar-links")[0]
 
+
+// Create date for copyright footer
+
+$("#copyright").text(new Date().getFullYear());
+
+
 toggleButton.addEventListener("click", () => {
     navBarLinks.classList.toggle("active")
 })
