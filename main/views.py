cebca56307ff0ab1e932a3d03745dda9ec7e1b49
Johneriksson88 <john.e.eriksson@gmail.com>
2022-11-09 21:45:39 +0000

    Create app for main page

---
+++ main/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse, Http404
+
+def index(request):
+    return render(request, 'index.html')
+
+def section(request, num):
+    if 1 <= num <= 3:
+        return HttpResponse(texts[num-1])
+    else:
+        raise Http404("No such section")
