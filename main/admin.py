54350788b3fa9462d2dea5d857641d31f04a024b
Johneriksson88 <john.e.eriksson@gmail.com>
2022-12-20 23:06:20 +0000

    Add list display columns to admin page

--- main/admin.py
+++ main/admin.py
@@ -3,4 +3,19 @@ from .models import StorageUnit, Customer, Order
 
 
 my_models = [StorageUnit, Customer, Order]
-admin.site.register(my_models)
+""" admin.site.register(my_models) """
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("pk", "fullname", "email")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("pk", "customer", "start_date")
+
+
+@admin.register(StorageUnit)
+class StorageUnitAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
